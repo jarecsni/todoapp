b9e453a1f34c705ba24fa0946996d7aa7b658a8f
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2022-07-27 08:36:34 +0100

    Adding lists of todos

--- app.py
+++ app.py
@@ -15,13 +15,27 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, server_default='false')
-
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+class TodoList(db.Model):
+    __tablename__ = 'todolists';
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
+
+@app.route('/lists/<list_id>')
+def list_of_todos(list_id):
+    return render_template('index.html', 
+    lists=TodoList.query.all(),
+    active_list=TodoList.query.get(list_id),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('list_of_todos', list_id=1))
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
